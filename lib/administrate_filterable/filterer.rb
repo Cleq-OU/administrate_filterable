9cf45e06375c87a843124f9727769f8d0af087d1
Ukaza Perdana <ukaza@perdana.dev>
2025-12-12 08:18:37 +0800

    Fix date filtering

--- lib/administrate_filterable/filterer.rb
+++ lib/administrate_filterable/filterer.rb
@@ -26,15 +26,34 @@ module AdministrateFilterable
       filter_params = params[resource_name]
       return resources if filter_params.blank?
 
+      # Get date/datetime field names from dashboard
+      date_fields = @filterable_attributes.select do |attr|
+        attr.is_a?(Administrate::Field::DateTime) || attr.is_a?(Administrate::Field::Date)
+      end.map { |attr| attr.attribute.to_s }
+
+      # Process date range filters first
+      date_fields.each do |field|
+        from_val = filter_params["#{field}_from"]
+        to_val = filter_params["#{field}_to"]
+
+        resources = resources.where("#{field} >= ?", from_val) if from_val.present?
+        resources = resources.where("#{field} <= ?", to_val) if to_val.present?
+      end
+
+      # Process regular filters (excluding date range params)
       filter_params.each do |key, value|
-        next unless resources.column_names.include?(key.to_s) && value.present?
+        key_str = key.to_s
 
-        # TODO: Add support for relational filter (e.g. filter by `belongs_to` association, etc)
-        column = resources.columns_hash[key.to_s]
+        # Skip date range params (already processed)
+        next if date_fields.any? { |field| key_str == "#{field}_from" || key_str == "#{field}_to" }
 
-        # Use LIKE for string columns, exact match for others (integer, enum, date, etc)
+        # Regular filters
+        next unless resources.column_names.include?(key_str) && value.present?
+
+        column = resources.columns_hash[key_str]
+
         if column && column.type == :string
-          sanitized_query = ActiveRecord::Base.send(:sanitize_sql_array, ["#{key} LIKE ?", "%#{value}%"])
+          sanitized_query = ActiveRecord::Base.send(:sanitize_sql_array, ["#{key_str} LIKE ?", "%#{value}%"])
           resources = resources.where(sanitized_query)
         else
           resources = resources.where(key => value)
